eb9df03a76ce1e52e91973d9d5b90a55e6d5fb76
DevAlysonh <dev.alysonh@gmail.com>
2022-07-23 16:26:45 -0300

    Add validate and columns on people.java

--- src/main/java/com/peoples/apirest/entities/People.java
+++ src/main/java/com/peoples/apirest/entities/People.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class People implements Serializable {
@@ -20,7 +22,13 @@ public class People implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	
+	@Column(length = 255)
+	@NotBlank
 	private String name;
+	
+	@Column(length = 10)
 	private String birthDate;
 	
 	@OneToMany(mappedBy = "people")
--- src/main/java/com/peoples/apirest/resources/PeopleResource.java
+++ src/main/java/com/peoples/apirest/resources/PeopleResource.java
@@ -1,11 +1,11 @@
 package com.peoples.apirest.resources;
 
-import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.peoples.apirest.entities.Address;
 import com.peoples.apirest.entities.People;
 import com.peoples.apirest.services.AddressService;
 import com.peoples.apirest.services.PeopleService;
@@ -46,7 +44,7 @@ public class PeopleResource {
 
 	@PostMapping(value = "/pessoas")
 	@ResponseStatus(HttpStatus.CREATED)
-	public People create(@RequestBody People people) {
+	public People create(@RequestBody @Valid People people) {
 		return service.createPeople(people);
 	}
 
