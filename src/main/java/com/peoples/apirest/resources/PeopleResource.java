7ad4bf8b9be60b09c0b71e01e287a17546325db1
DevAlysonh <dev.alysonh@gmail.com>
2022-07-23 16:53:12 -0300

    Add return validation errors method in PeopleResource

--- src/main/java/com/peoples/apirest/resources/PeopleResource.java
+++ src/main/java/com/peoples/apirest/resources/PeopleResource.java
@@ -1,12 +1,17 @@
 package com.peoples.apirest.resources;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +63,22 @@ public class PeopleResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		service.deletePeople(id);
+	}
+	
+	
+	/*Metodo para retornar os erros de form validation*/
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
+		Map<String, String> errors = new HashMap<>();
+		
+		exception.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		
+		return errors;
 	}
 
 }
