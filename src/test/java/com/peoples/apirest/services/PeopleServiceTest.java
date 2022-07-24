1ab37007aba91132e88ae8e2a26fa94be2b7cf2f
DevAlysonh <dev.alysonh@gmail.com>
2022-07-23 23:41:03 -0300

    Create PeopleServiceTest and findAllPeopleTest method

---
+++ src/test/java/com/peoples/apirest/services/PeopleServiceTest.java
@@ -0,0 +1,41 @@
+package com.peoples.apirest.services;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.peoples.apirest.entities.Address;
+import com.peoples.apirest.entities.People;
+import com.peoples.apirest.repositories.PeopleRepository;
+
+@ExtendWith(MockitoExtension.class)
+class PeopleServiceTest {
+
+	@InjectMocks
+	private PeopleService service;
+
+	@Mock
+	private PeopleRepository repository;
+
+	@Test
+	void findAllPeopleTest() {
+		Long id = 1L;
+		People people = new People(id, "Alyson Henrique", LocalDate.now());
+		List<Address> addresses = List.of(new Address(id, "Rua 01", "58052188", "22", "João Pesoa", false));
+		people.setAddress(addresses);
+		List<People> peopleList = List.of(people);
+		
+		Mockito.when(repository.findAll()).thenReturn(peopleList);
+		
+		Assertions.assertNotNull(service.findAll());
+		
+	}
+
+}
